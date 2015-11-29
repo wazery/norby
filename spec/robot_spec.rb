b89a0f8a2e49d3ac7e5edb211b4a42bf3a948597
Islam Wazery <wazery@ubuntu.com>
2015-11-29 03:35:07 +0200

    Setup RSpec for the gem

---
+++ spec/robot_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Norby::Robot do
+  it 'should return true' do
+    expect(true).to eq true
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,6 @@
+require 'rspec'
+require 'norby'
+
+RSpec.configure do |config|
+  config.color     = true
+end
