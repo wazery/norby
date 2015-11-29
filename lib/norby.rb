aab684d28f868d0b82709b15aa03d1a4c6dbe313
Islam Wazery <wazery@ubuntu.com>
2015-11-29 03:05:21 +0200

    Refactor the code
    - Handle the ignoring for every command other than place

--- lib/norby.rb
+++ lib/norby.rb
@@ -1,4 +1,5 @@
 require 'norby/version'
+require 'thor'
 require 'colorize'
 
 # The wrapper module for the gem
@@ -6,11 +7,12 @@ module Norby
   DIRECTIONS = { 'NORTH' => 0, 'EAST' => 1, 'SOUTH' => 2, 'WEST' => 3 }
 
   # Actual Robot logic
-  class Robot
+  class Robot # < Thor
     def initialize
       @curr_place_x = 0
       @curr_place_y = 0
       @curr_dir = 0
+      @place_not_executed = true
     end
 
     def start
@@ -29,30 +31,33 @@ module Norby
     private
 
     def issue_commands(inputs)
-      inputs.each_with_index do |input, index|
-        if index == 0
-          parse_input(input)
+      inputs.each do |input|
+        if @place_not_executed
+          parse_place_input(input)
         else
-          meth_name = input.downcase
-          send(meth_name) # if respond_to?(meth_name.to_sym)
+          begin
+            send(input.downcase.to_sym)
+            @place_not_executed = true
+          rescue
+            puts 'Something went wrong with the inserted command, exiting..'
+              .colorize(:red)
+          end
         end
       end
     end
 
-    def parse_input(input)
-      if /^PLACE [0-4],[0-4],(NORTH|EAST|SOUTH|WEST)$/ =~ input
-        arr = input.split(',')
-        place(arr[0].split(' ')[1].to_i, arr[1].to_i, arr[2])
-      else
-        puts 'Somthing went wrong with PLACE command, exiting..'
-          .colorize(:red)
-      end
+    def parse_place_input(input)
+      return unless /^PLACE [0-4],[0-4],(NORTH|EAST|SOUTH|WEST)$/ =~ input
+
+      arr = input.split(',')
+      place(arr[0].split(' ')[1].to_i, arr[1].to_i, arr[2])
     end
 
     def place(x, y, dir)
       @curr_place_x = x
       @curr_place_y = y
       @curr_dir = DIRECTIONS[dir]
+      @place_not_executed = false
     end
 
     def move
@@ -65,7 +70,7 @@ module Norby
     end
 
     def right
-      new_dir = @curr_dir - 1
+      new_dir = @curr_dir + 1
       new_dir > 3 ? @curr_dir = 0 : @curr_dir = new_dir
     end
 
@@ -79,11 +84,10 @@ module Norby
         .colorize(:green)
     end
 
-    def change_place(new_pos, operation)
+    def change_place(max, operation)
       new_place = 0
       @curr_dir == 0 ? axis = :y : axis = :x
-      eval("new_place = @curr_place_#{axis} #{operation} 1")
-      eval("@curr_place_#{axis} = new_place") if new_place < new_pos 
+      eval("@curr_place_#{axis} = @curr_place_#{axis} #{operation} 1") if new_place < max
     end
   end
 end
--- norby.gemspec
+++ norby.gemspec
@@ -12,7 +12,7 @@ Gem::Specification.new do |spec|
 
   spec.summary       = 'A simple robot simulator written in Ruby'
   spec.description   = 'A simple robot simulator written in Ruby'
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.homepage      = 'https://www.github.com/wazery/norby'
 
   spec.files         = `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
   spec.bindir        = 'exe'
@@ -20,6 +20,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = ['lib']
 
   spec.add_dependency 'colorize'
+  spec.add_dependency 'thor'
 
   spec.add_development_dependency 'bundler', '~> 1.10'
   spec.add_development_dependency 'rake', '~> 10.0'
